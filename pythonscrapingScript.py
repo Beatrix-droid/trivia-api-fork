aeabded0515f117783fb1b491bd07e6fa554c73f
Beatrice <beatrice.federici123@gmail.com>
2023-09-07 15:38:03 +0000

    added link to quiz generator

--- pythonscrapingScript.py
+++ pythonscrapingScript.py
@@ -27,12 +27,27 @@ from time import sleep
 # with open ("questions.json", "w") as file:
 #         file.write(json_questions)
 
-with open ("public/questions/italianTranslation.json", "r") as jsonfile:
-    data=json.load(jsonfile)
-    with open("domande.txt", "w") as textfile:
-        textfile.write("totale domande: "+ str(len(data))+ "\n")
-        for question in data:
-            textfile.write("\n")
-            textfile.write(f"{data.index(question)+ 1}:  {question['question']['text']} \n")
-            textfile.write(f"   risposta corretta: {question['correctAnswer']} \n")
-            textfile.write(f"   risposte sbagliate: {question['incorrectAnswers']} \n")
+# with open ("public/questions/italianTranslation.json", "r") as jsonfile:
+#     data=json.load(jsonfile)
+#     with open("domande.txt", "w") as textfile:
+#         textfile.write("totale domande: "+ str(len(data))+ "\n")
+#         for question in data:
+#             textfile.write("\n")
+#             textfile.write(f"{data.index(question)+ 1}:  {question['question']['text']} \n")
+#             textfile.write(f"   risposta corretta: {question['correctAnswer']} \n")
+#             textfile.write(f"   risposte sbagliate: {question['incorrectAnswers']} \n")
+
+r=requests.get("https://server.opexams.com/quiz-data/EkUQ0vM8wX2", timeout=5)
+data=r.json()
+print(data)
+
+# with open("domande-Lokal.txt", "w") as textfile:
+#         textfile.write("totale domande: "+ str(len(data["questions"]))+ "\n")
+#         for question in data["questions"]:
+#             questionBody=question["question"]
+#             answer=question["answer"]
+#             wrong_answers=question["options"]["value"]
+#             textfile.write("\n")
+#             textfile.write(f"{data['questions'].index(question)+ 1}:  {questionBody} \n")
+#             textfile.write(f"   risposta corretta: {answer} \n")
+#             textfile.write(f"   risposte sbagliate: {wrong_answers} \n")
