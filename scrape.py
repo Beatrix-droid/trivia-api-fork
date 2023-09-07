8e93f7cb14851b7c15ddac4279e49caca2c14aa9
Beatrice <beatrice.federici123@gmail.com>
2023-09-07 10:29:50 +0000

    added more questions

--- scrape.py
+++ scrape.py
@@ -3,26 +3,30 @@ import json
 from time import sleep
 
 
-url="https://the-trivia-api.com/v2/questions?tags=cooking&limit=1"
+url="https://the-trivia-api.com/v2/questions?tags=food&limit=1"
 
 questions=[]
 i = 0
-while i < 40:
+while i < 300:
 
-    r= requests.get(url, timeout=5)
-    question= r.json()[0]
-    print(question)
+        r= requests.get(url, timeout=5)
+        question= r.json()[0]
+        print(question)
 
-    if question in questions:
-        print("it's already here")
-    else:
-        print("new question, adding it to the list if questions")
-        questions.append(question)
-    i+=1
-    print(i)
-    sleep(2)
+        if question in questions:
+            print("it's already here")
+        else:
+            print("new question, adding it to the list if questions")
+            questions.append(question)
+        i+=1
+        print(i)
+        sleep(2)
 
-# write the set of questions to a json file
+    # write the set of questions to a json file
 json_questions=json.dumps(questions, indent=4)
 with open ("questions.json", "w") as file:
-     file.write(json_questions)
+        file.write(json_questions)
+
+# with open ("italianTranslation.json", "r") as file:
+#     data=json.load(file)
+#     print(len(data))
